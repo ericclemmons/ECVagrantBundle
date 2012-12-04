db88661d12eb90628fe6f94e7d313eb54638945b
Paul Seiffert <paul.seiffert@mayflower.de>
2012-12-04 03:21:37 +0100

    Fixed test environment

--- Tests/bootstrap.php
+++ Tests/bootstrap.php
@@ -1,10 +1,20 @@
 <?php
 
-require_once $_SERVER['SYMFONY_PATH'] . '/Symfony/Component/ClassLoader/UniversalClassLoader.php';
+if (!isset($_SERVER['VENDOR_PATH'])) {
+    throw new \InvalidArgumentException('No VENDOR_PATH provided.');
+}
+
+$vendorPath = $_SERVER['VENDOR_PATH'];
+
+$symfonyPath = $vendorPath . '/symfony/symfony/src/';
+$guzzlePath = $vendorPath . '/guzzle/guzzle/src/';
+
+require_once $symfonyPath . 'Symfony/Component/ClassLoader/UniversalClassLoader.php';
 
 use Symfony\Component\ClassLoader\UniversalClassLoader;
 
 $loader = new UniversalClassLoader();
-$loader->registerNamespace('Symfony', $_SERVER['SYMFONY_PATH']);
-$loader->registerNamespace('EC\\Bundle\\VagrantBundle', realpath(dirname(__FILE__) . '/../'));
+$loader->registerNamespace('Symfony', $symfonyPath);
+$loader->registerNamespace('Guzzle', $guzzlePath);
+$loader->registerNamespace('EC\\Bundle\\VagrantBundle', realpath(dirname(__FILE__) . '/../../../../'));
 $loader->register();
