19a134246d2839b6e0c72574bf49ef86b7c6fb9b
Paul Seiffert <paul.seiffert@mayflower.de>
2012-12-17 12:34:03 +0100

    Testing infrastructure

--- Tests/bootstrap.php
+++ Tests/bootstrap.php
@@ -1,24 +1,3 @@
 <?php
 
-if (!isset($_SERVER['VENDOR_PATH'])) {
-    throw new \InvalidArgumentException('No VENDOR_PATH provided.');
-}
-
-$vendorPath = $_SERVER['VENDOR_PATH'];
-
-$symfonyPath = $vendorPath . '/symfony/symfony/src/';
-$guzzlePath = $vendorPath . '/guzzle/guzzle/src/';
-$sensioGeneratorPath = $vendorPath . '/sensio/generator-bundle/';
-$twigPath = $vendorPath . '/twig/twig/lib';
-
-require_once $symfonyPath . 'Symfony/Component/ClassLoader/UniversalClassLoader.php';
-
-use Symfony\Component\ClassLoader\UniversalClassLoader;
-
-$loader = new UniversalClassLoader();
-$loader->registerNamespace('Symfony', $symfonyPath);
-$loader->registerNamespace('Guzzle', $guzzlePath);
-$loader->registerNamespace('Sensio\\Bundle\\GeneratorBundle', $sensioGeneratorPath);
-$loader->registerPrefix('Twig_', $twigPath);
-$loader->registerNamespace('EC\\Bundle\\VagrantBundle', realpath(dirname(__FILE__) . '/../../../../'));
-$loader->register();
+require_once __DIR__.'/../vendor/autoload.php';
